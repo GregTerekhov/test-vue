05b807353aed3b94aa385084be6cd78e91fb9281
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-01-25 09:57:41 +0200

    fix: add section tag into HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,12 +10,14 @@ const { movies, isLoading } = storeToRefs(store)
 
 <template>
   <main>
-    <div class="container mx-auto px-8 md:px-16">
-      <h1 class="mb-10 text-3xl font-bold">Сеанси</h1>
-      <TheMoviesList v-if="movies && movies.length > 0 && !isLoading" :movies="movies" />
-      <div v-else class="flex h-full items-center justify-center">
-        <v-icon name="fa-spinner" fill="#2da3d3" animation="spin" speed="2" />
+    <section class="py-10 lg:pb-36">
+      <div class="container mx-auto px-8 md:px-16">
+        <h1 class="mb-10 text-3xl font-bold">Сеанси</h1>
+        <TheMoviesList v-if="movies && movies.length > 0 && !isLoading" :movies="movies" />
+        <div v-else class="flex h-full items-center justify-center">
+          <v-icon name="fa-spinner" fill="#2da3d3" animation="spin" speed="2" />
+        </div>
       </div>
-    </div>
+    </section>
   </main>
 </template>
